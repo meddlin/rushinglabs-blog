441559edfb0064b64b96732036e895af89e94cc4
meddlin <drushing.dev@gmail.com>
2021-11-24 16:28:26 -0600

    Configured linting for Next.js 12
    - Setup eslint ignoreDuringBuilds, eslint errors don't affect deploys
    - Changed eslint to base, from strict

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
  * So, we need this config to extend that behavior.
  */
 module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        // Taken from: https://nextjs.org/docs/api-reference/next.config.js/ignoring-eslint
+        ignoreDuringBuilds: true,
+    },
     images: {
         domains: ['meddlin-web.s3.us-east-2.amazonaws.com']
     }
