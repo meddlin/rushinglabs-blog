9d10ccf02535af292b032080082bd915eabbd1d2
meddlin <drushing.dev@gmail.com>
2021-11-23 23:50:01 -0500

    Section on cpat category
    - Not sure how it happened, but had to fix the post link on here, too

--- pages/categories/cpat/index.js
+++ pages/categories/cpat/index.js
@@ -44,18 +44,20 @@ export default function CpatSection({ posts, prevPosts, nextPosts, categoriesLis
                 <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.postsSection}`}>
                     <ul className={utilStyles.list}>
                         {(posts && posts.length > 0) ? (
-                            posts.map( ({ id, year, date, title, preview }) => (
+                            posts.map( ({ id, year, date, title, preview, section }) => (
                                 <li className={utilStyles.listItem} key={id}>
                                     <Link href={`/blog/${year}/${id}`}>
                                         <a>{title}</a>
                                     </Link>
                                     <br />
-                                    <small className={utilStyles.lightText}>
+                                    <small className={utilStyles.subpreview}>
+                                        {section ? (<text>{section}&nbsp;&mdash;&nbsp;</text>) 
+                                            : ''}
                                         <Date dateString={date} />
                                     </small>
                                     <p>{preview}</p>
 
-                                    <Link href={`/blog/${id}`}>
+                                    <Link href={`/blog/${year}/${id}`}>
                                         <a className={utilStyles.readMoreLink}>Read More &mdash;&gt;</a>	
                                     </Link>
                                 </li>
