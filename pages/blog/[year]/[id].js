24d0be30d79e4c4bebfa1e6a6e7ef89faa895f9f
meddlin <drushing.dev@gmail.com>
2021-12-07 02:35:06 -0600

    MDX works...properly
    - /blog/[year]/[id].js - refitted to use MDXRemote, remove broken calls for dynamic imports, and pulls in components used in MDX content
    - /lib/posts.js - refactored to query for .mdx files, and removed calls for remark to generate HTML from Markdown...also, a stupid amount of console.log()
    - Using '/posts/2021/test-post.mdx' as a test for showing React components within MDX/Markdown
    - Changed all of the .md files to .mdx

--- pages/blog/[year]/[id].js
+++ pages/blog/[year]/[id].js
@@ -1,10 +1,16 @@
-import dynamic from 'next/dynamic';
+import { MDXRemote } from 'next-mdx-remote';
 import Layout from '../../../components/layout';
 import Head from 'next/head';
 import Date from '../../../components/date';
 import { getAllPostIds, getPostData } from '../../../lib/posts'; //  '../../../../lib/posts';
 import utilStyles from '../../../styles/utils.module.css';
 import postStyles from '../../../styles/post.module.css';
+
+/**
+ * Importing the components which will be used in our MDX posts
+ */
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import Button from '../../../components/button';
 
 /**
  * getStaticPaths is required by Next.js
@@ -16,9 +22,7 @@ export async function getStaticPaths() {
 
     // The params building happens in /lib/posts.getAllPostIds()
     return {
-        paths: paths.map( (path) => ({
-            params: { id: path.params.id, year: path.params.year }
-        })),
+        paths: paths,
         fallback: false
     }
 }
@@ -28,7 +32,7 @@ export async function getStaticPaths() {
  * 
  * See: https://nextjs.org/docs/basic-features/data-fetching
  */
-export async function getStaticProps({ params }) {
+export const getStaticProps = async ({ params }) => {
     // `params` contains the router parameters for pages using dynamic routes
     // For this route we have the directory structure /blog/[year]/[id].js
     // --> So, `year` and `id` are part of the params object <--
@@ -38,41 +42,42 @@ export async function getStaticProps({ params }) {
     console.log(`getStaticProps: ${year}`);
     console.log(`getStaticProps: ${postId}`)
 
-    /* const postData = await getPostData(params.year, params.id); */
-    /* return {
-        props: {
-            postData
-        }
-    } */
-    const postData = require(`../../../posts/${year}/${postId}.mdx`);
-    console.log(postData);
+    const postData = await getPostData(year, postId);
 
     return {
         props: {
-            postId,
-            year,
-            metadata: require(`../../../posts/${year}/${postId}.mdx`).metadata, // watch for .mdx extension
-        },
-    };
+            postData
+        }
+    }
 }
 
-export default function Post({ postId, year, metadata }) {
-    let mdx;
-    const Mdx = dynamic(() => import(`../../../posts/${year}/${postId}.mdx`)); // watch for .mdx extension
-    mdx = <Mdx />
+const Post = (props) => {
+
+    // console.log(`Post - props: ${JSON.stringify(props)}`);
+
+    // console.log(`Post - id: ${props.postData.id}`);
+    // console.log(`Post - postData: ${JSON.stringify(props.postData)}`);
+    // console.log(`Post - mdxSource: ${JSON.stringify(props.postData.mdxSource)}`);
+
+    let mdx = props.postData.mdxSource;
+    let title = props.postData.title;
+    let date = props.postData.date;
+    let section = props.postData.section;
 
     return (
         <Layout>
             <Head>
-                <title>{metadata.title}</title>
+                {/* <title>{metadata.title}</title> */}
             </Head>
             
             <article className={postStyles.content}>
-                <h1 className={utilStyles.headingX1}>{metadata.title}</h1>
-                <Date dateString={metadata.date} />
+                <h1 className={utilStyles.headingX1}>{title}</h1>
+                <Date dateString={date} />
                 {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
-                {mdx}
+                <MDXRemote {...mdx} components={{ Button, SyntaxHighlighter }} />
             </article>
         </Layout>
     );
 }
+
+export default Post;
