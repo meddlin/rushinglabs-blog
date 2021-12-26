85b385d611f49fc0491b7eb53f2e6ec452973f01
meddlin <drushing.dev@gmail.com>
2021-12-26 00:42:31 -0600

    Docs mechanics are starting to work
    - Added a few 'docs' functions to the /lib/posts utility
    - Docs index page (main docs view) can pull posts from the /docs directory and display them
    - Main docs view also shows (rudimentary) link for the "/docs/owasp" section
    - Docs "section view" route works...no specific data moved onto it yet though

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,9 +1,82 @@
-import Layout from '../../components/layout';
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import { getAllDocsSections, getSortedDocsPostsData } from '../../lib/posts';
+import config from '../../blogConfig';
 
-function Docs() {
+export async function getStaticProps() {
+    const docsData = getSortedDocsPostsData();
+    const sections = getAllDocsSections();
+
+    // Paging information
+	const startIndex = 0;
+	const endIndex = config.postsPerPage;
+	const prevPosts = null;
+	const nextPosts = (endIndex >= docsData.length) ? null : 2;
+
+	return {
+		props: {
+			docs: docsData.slice(startIndex, endIndex),
+			sections,
+			prevPosts,
+			nextPosts
+		}
+	}
+}
+
+function Docs({ docs, sections, prevPosts, nextPosts }) {
     return (
         <Layout>
-            <h1>Docs is a space under active development.</h1>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+
+            <section>
+                <h1>Docs is a space under active development.</h1>
+            </section>
+
+            <sections>
+                <ul className={utilStyles.list}>
+                    {sections.map( section => (
+                        <li className={utilStyles.listItem} key={section}>
+                            <Link href={`/docs/${section}`}>
+                                <a>{section}</a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </sections>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.postsSection}`}>
+                <ul className={utilStyles.list}>
+                    {docs.map( ({ id, date, title, preview }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/docs/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <small className={utilStyles.subpreview}>
+                                {date ? (<Date dateString={date} />) : ''}
+                            </small>
+                            <p>{preview}</p>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+
+            <section className={`${utilStyles.centeredButtons}`}>
+                {prevPosts !== null && (
+                    <Link href={"/docs/pages/" + prevPosts} passHref>
+                        <a>« newer</a>
+                    </Link>
+                )}
+                {nextPosts !== null && (
+                    <Link href={"/docs/pages/" + nextPosts} passHref>
+                    <a>older »</a>
+                    </Link>
+                )}
+            </section>
         </Layout>
     );
 }
