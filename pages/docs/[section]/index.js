be78ee7c15aa9ef5ff5e61f3edc86c640c6c7df9
meddlin <drushing.dev@gmail.com>
2021-12-26 00:42:31 -0600

    Docs mechanics are starting to work
    - Added a few 'docs' functions to the /lib/posts utility
    - Docs index page (main docs view) can pull posts from the /docs directory and display them
    - Main docs view also shows (rudimentary) link for the "/docs/owasp" section
    - Docs "section view" route works...no specific data moved onto it yet though

---
+++ pages/docs/[section]/index.js
@@ -0,0 +1,13 @@
+import Layout from '../../../components/layout';
+
+const section = 'owasp'
+
+function DocsSection() {
+    return (
+        <Layout>
+            <h1>Docs: {section}</h1>
+        </Layout>
+    );
+}
+
+export default DocsSection;
