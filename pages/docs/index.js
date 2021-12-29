10eb47cf4bfe66a36cc0326fce0ceb0ddc20486f
meddlin <drushing.dev@gmail.com>
2021-12-28 23:19:55 -0600

    No pagination in docs, no need for blogConfig

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -4,7 +4,6 @@ import Layout, { siteTitle } from '../../components/layout';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 import { getAllDocsSections, getPostsFromDocsSubdir, getSortedDocsPostsData } from '../../lib/docs';
-import config from '../../blogConfig';
 
 export async function getStaticProps() {
     const docsData = getSortedDocsPostsData();
