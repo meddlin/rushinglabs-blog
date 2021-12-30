f3f01515189bc6e3e26a7f8dcbd8436387967390
meddlin <drushing.dev@gmail.com>
2021-12-30 00:32:36 -0600

    Updated /projects/index.js
    - The main projects view now works with all of the data and new logic
    - Even support for published/non-published projects in the listing

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -39,54 +39,41 @@ const Projects = ({ metadata, projects }) => {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.postsSection}`}>
                 <h3>New projects view</h3>
                 <ul>
-                    {metadata && metadata.length > 0 ? (metadata.map( meta => (
-                        <li key={meta.title}>
-                            <p>
-                                <a href={`/projects/${meta.title}`}>
-                                    {meta.title}
-                                </a>
-                            </p>
-                            <p>{meta.summary}</p>
-                            <p>
-                                {meta.links.map( link => (
-                                    <a href={link.url}>{link.text}</a>
-                                ))}
-                            </p>
-                            <p>
-
-                            </p>
-                        </li>
-                    ))) : ''}
+                    {metadata && metadata.length > 0 ? 
+                        (metadata.map( meta => (
+                            (meta.published ? (
+                                <li key={meta.title}>
+                                    <div>
+                                        {meta.images && (meta.images.length && meta.images[0].url !== '') > 0 ? (
+                                            <Image 
+                                                src={meta.images[0].url}
+                                                alt={meta.images[0].alt}
+                                                height={meta.images[0].height}
+                                                width={meta.images[0].width}
+                                            />
+                                        ) : ''}
+                                        <div>
+                                            <p>
+                                                <a href={`/projects/${meta.title}`}>
+                                                    {meta.title}
+                                                </a>
+                                            </p>
+                                            <p>{meta.summary}</p>
+                                            <p>
+                                                {meta.links.map( link => (
+                                                    <a href={link.url}>{link.text}</a>
+                                                ))}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </li>
+                            ) : '')
+                        )
+                    )) : ''}
                 </ul>
 
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                        <div style={{ width: '80%' }}>
-                            <h2>Amortization Calculator</h2>
-                            <a href="https://amortize-client.herokuapp.com/">
-                                <Image src="https://meddlin-web.s3.us-east-2.amazonaws.com/post_2021-09-24/amortize-demo-screenshot.png"
-                                alt="Boring screenshot of an amortization table" height={300} width={450} />
-                            </a>
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                            <p>
-                                While shopping for a house, I needed an amortization
-                                calculator that was more helpful to my budget than
-                                what I was finding online. So, I made one.
-                            </p>
-                            <p>
-                                I also took the opportunity to test what kind of CI/CD
-                                pipeline&mdash;with security tests&mdash;can be created (for free)
-                                with the built-in tools available on GitHub. The demo 
-                                is hosted on Heroku via Docker containers.
-                            </p>
-                            <div>
-                                <a href="https://github.com/meddlin/amortize">Check the code on GitHub!</a> &nbsp; | &nbsp;
-                                <a href="https://meddlin.github.io/amortize/">Project Site</a> &nbsp; | &nbsp;
-                                <a href="https://amortize-client.herokuapp.com/">Demo</a>
-                            </div>
-                        </div>
-                    </div>
+                {/* <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    
                     <div style={{ display: 'flex', flexDirection: 'row' }}>
                         <div style={{ width: '60%' }}>
                             <h2>Rushing Labs (website)</h2>
@@ -110,7 +97,7 @@ const Projects = ({ metadata, projects }) => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </section>
         </Layout>
     );
--- projectsConfig.js
+++ projectsConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
     projectsMeta: [
         {
-            title: 'amortize',
-            summary: "While shopping for a house, I needed an amortization calculator that was more helpful to my budget than what I was finding online. So, I made one.",
+            title: 'Amortization Calculator',
+            published: true,
+            summary: "While shopping for a house, I needed an amortization calculator that was more helpful to my budget than what I was finding online. So, I made one. I also took the opportunity to test what kind of CI/CD pipeline--with security tests--can be created (for free) with the built-in tools available on GitHub. The demo is hosted on Heroku via Docker containers.",
             links: [
                 {
                     text: 'Check the code on GitHub!',
@@ -19,13 +20,29 @@ module.exports = {
             ],
             images: [
                 {
-                    size: '',
-                    url: ''
+                    size: 'lg',
+                    url: 'https://meddlin-web.s3.us-east-2.amazonaws.com/post_2021-09-24/amortize-demo-screenshot.png',
+                    alt: "Boring screenshot of an amortization table",
+                    height: 300,
+                    width: 450
                 }
             ]
         },
         {
+            title: "Rushing Labs (website)",
+            published: true,
+            summary: "Originally, I was just playing around with hosting a blog, and over time it has ballooned into a happy little place to share some of the developery stuff I do.",
+            links: [
+                {
+                    text: "Check the code on GitHub",
+                    url: "https://github.com/meddlin/rushinglabs-blog-nextjs"
+                }
+            ],
+            images: []
+        },
+        {
             title: 'cpat',
+            published: false,
             summary: '',
             links: [
                 {
@@ -42,11 +59,12 @@ module.exports = {
         },
         {
             title: 'owasp',
-            summary: '',
+            published: true,
+            summary: 'OWASP Top 10 is considered the "gateway to AppSec", so naturally I\'m interested. But quickly I found it confusing to read about generic security vulnerabilities when I was used to code snippets and hands-on examples as a developer. So, I brought the two ideas together: OWASP Top 10 + hands-on code.',
             links: [
                 {
-                    text: '',
-                    url: ''
+                    text: 'Click here for the articles',
+                    url: 'https://rushinglabs.com/projects/owasp'
                 }
             ],
             images: [
