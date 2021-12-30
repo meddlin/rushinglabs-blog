63c083726ab23090b04e0fb1ac89a7c191f75313
meddlin <drushing.dev@gmail.com>
2021-12-30 00:32:36 -0600

    Updated /projects/index.js
    - The main projects view now works with all of the data and new logic
    - Even support for published/non-published projects in the listing

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -93,34 +93,8 @@ const Projects = ({ metadata, projects }) => {
                     ))) : ''}
                 </ul>
 
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                        <div style={{ width: '80%' }}>
-                            <h2>Amortization Calculator</h2>
-                            <a href="https://amortize-client.herokuapp.com/">
-                                <Image src="https://meddlin-web.s3.us-east-2.amazonaws.com/post_2021-09-24/amortize-demo-screenshot.png"
-                                alt="Boring screenshot of an amortization table" height={300} width={450} />
-                            </a>
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                            <p>
-                                While shopping for a house, I needed an amortization
-                                calculator that was more helpful to my budget than
-                                what I was finding online. So, I made one.
-                            </p>
-                            <p>
-                                I also took the opportunity to test what kind of CI/CD
-                                pipeline&mdash;with security tests&mdash;can be created (for free)
-                                with the built-in tools available on GitHub. The demo 
-                                is hosted on Heroku via Docker containers.
-                            </p>
-                            <div>
-                                <a href="https://github.com/meddlin/amortize">Check the code on GitHub!</a> &nbsp; | &nbsp;
-                                <a href="https://meddlin.github.io/amortize/">Project Site</a> &nbsp; | &nbsp;
-                                <a href="https://amortize-client.herokuapp.com/">Demo</a>
-                            </div>
-                        </div>
-                    </div>
+                {/* <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    
                     <div style={{ display: 'flex', flexDirection: 'row' }}>
                         <div style={{ width: '60%' }}>
                             <h2>Rushing Labs (website)</h2>
