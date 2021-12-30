99448a00cec65c69de4ddc0cd9d7a924c21dd3a5
meddlin <drushing.dev@gmail.com>
2021-12-29 23:06:25 -0600

    Projects section main functionality works
    - the /projects/[project] route works (viewing all a project has to offer)
    - the /projects/[project]/[id].js route works (for viewing specific posts within a project)

---
+++ pages/projects/[project]/index.js
@@ -0,0 +1,50 @@
+import Layout from '../../../components/layout';
+import { getPostsFromProjectsSubdir, getAllProjects } from '../../../lib/projects';
+
+export async function getStaticProps({ params }) {
+    const projectPosts = getPostsFromProjectsSubdir(params.project);
+
+    return {
+        props: {
+            projectTitle: params.project,
+            posts: projectPosts
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const projects = getAllProjects();
+
+    return {
+        paths: projects.map(proj => {
+            return {
+                params: { project: proj }
+            }
+        }),
+        fallback: false
+    }
+}
+
+const ProjectsSection = ({ projectTitle, posts }) => {
+    return (
+        <Layout>
+            <h1>Project: {projectTitle}</h1>
+
+            {posts ? (
+                <ul>
+                {
+                    posts.map( post => (
+                        <li key={post.title}>
+                            <a href={`/projects/${projectTitle}/${post.id}`}>
+                                {post.title}
+                            </a>
+                        </li>
+                    ))
+                }
+                </ul>
+            ) : ''}
+        </Layout>
+    );
+}
+
+export default ProjectsSection;
