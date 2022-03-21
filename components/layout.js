c6b3264affccb78039a469ac7094d67d766e3275
meddlin <drushing.dev@gmail.com>
2022-03-20 21:38:34 -0500

    Moved the <Head> configuration to own component
    - This is good for re-use in different layouts ;)

--- components/layout.js
+++ components/layout.js
@@ -1,34 +1,16 @@
-import Head from 'next/head';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import MenuIcon from './menu-icon';
 import NavbarLinks from './navbar-links';
+import LayoutHeadLoader from './layout-head-loader';
 
 const name = 'Rushing Labs';
-export const siteTitle = 'Rushing Labs';
 
 export default function Layout({ children, home }) {
     return (
       <div>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Rushing Labs - A place to build things and hangout"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.now.sh/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@400;700&family=Palanquin:wght@100;300&display=swap" rel="stylesheet"></link>
-        </Head>
+        <LayoutHeadLoader />
 
         <header className={styles.header}>
           <MenuIcon />
