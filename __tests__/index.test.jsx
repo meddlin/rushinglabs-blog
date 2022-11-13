7a5287cf0bf7ac2d1c939a23773354b064376037
meddlin <drushing.dev@gmail.com>
2022-11-13 04:22:50 -0600

    basic component test passes

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+    it('renders a heading', () => {
+        const samplePreviewPosts = [];
+
+        render(<Home previewPosts={samplePreviewPosts} />)
+    
+        // const heading = screen.getByRole('heading', {
+        //   name: /welcome to next\.js!/i,
+        // })
+        const text = screen.getByText("Hi! I'm Darrien, a software engineer and content creator based in Austin, TX.")
+    
+        expect(text).toBeInTheDocument()
+      })
+});
