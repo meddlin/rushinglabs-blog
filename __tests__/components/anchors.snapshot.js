896aa99c9b160e12538c7a74455ccb2b0e5dd3f3
meddlin <drushing.dev@gmail.com>
2022-11-27 14:07:38 -0600

    header anchors snapshot tests work

--- __tests__/components/anchors.snapshot.js
+++ __tests__/components/anchors.snapshot.js
@@ -1,9 +1,20 @@
 import { render } from '@testing-library/react';
-import H2 from '../../components/content-helpers/anchors';
+import { H2, H3, H4 } from '../../components/content-helpers/anchors';
 
 it('renders H2 unchanged', () => {
-    const content = "<h2>This is a test</h2>";
-    const { container } = render(<H2>This is a test</H2>);
+    const { container } = render(<H2>this is a test</H2>);
 
     expect(container).toMatchSnapshot()
-})
+});
+
+it('renders H3 unchanged', () => {
+    const { container } = render(<H3>this is a test</H3>);
+
+    expect(container).toMatchSnapshot()
+});
+
+it('renders H4 unchanged', () => {
+    const { container } = render(<H4>this is a test</H4>);
+
+    expect(container).toMatchSnapshot()
+});
