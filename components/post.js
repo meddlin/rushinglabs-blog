ff257ff327d97e70e71164abd9579a3ee63aa2fd
meddlin <drushing.dev@gmail.com>
2022-11-26 23:14:20 -0600

    Removed section from Post component

--- components/post.js
+++ components/post.js
@@ -1,21 +1,11 @@
-import Link from 'next/link'
-import Date from './date'
-import utilStyles from '../styles/utils.module.css'
-import PostPreviewImage from './post-preview-image';
+import Link from 'next/link';
 import escapeHTML from 'escape-html';
 
 export default function Post({ 
   id, 
   year,
-  date, 
   title, 
-  preview, 
-  previewImage, 
-  previewImageWidth, 
-  previewImageHeight, 
-  previewImageCreditText,
-  previewImageCreditUrl,
-  section 
+  preview
 }) {
   const articleLink = `/blog/${escapeHTML(year)}/${escapeHTML(id)}`;
 
@@ -26,13 +16,6 @@ export default function Post({
         <Link href={articleLink}>
           <a className="grow text-base text-black">{title}</a>
         </Link>
-
-        <span className="text-sm italic">
-          {section ? (
-              <a href={`/categories/${section}`} 
-                className="text-slate-700 decoration-indigo-500">{section}</a>
-            ) : ''}
-        </span>
       </div>
 
       <p className="text-sm text-slate-700">{preview}</p>
