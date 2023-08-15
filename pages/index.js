8098fddf2ad731f7e26ff7f10d626c208abbcd95
meddlin <drushing.dev@gmail.com>
2023-08-15 02:37:03 -0500

    notice to go to the blog

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Link from 'next/dist/client/link';
 import LandingLayout from '../components/landing/landing-layout';
 
 const Home = () => {
 	return (
 		<LandingLayout>
 			<h1 className="text-xl">An MDX docs site</h1>
+
+			<div className="flex flex-col justify-center">
+				<p>
+				<Link href="/blog">Go to blog</Link>
+				</p>
+				<p>More coming soon...</p>
+			</div>
 		</LandingLayout>
 	);
 };
